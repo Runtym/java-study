397da5b4551662b8c5fae71528e1d6112d9155df
Runtime <redfilaruntime@gmail.com>
2019-03-04 17:38:29 +0900

    test

---
+++ src/db/user2/AreaTest.java
@@ -0,0 +1,18 @@
+package db.user2;
+
+class Father{
+	void test() {
+		
+	}
+}
+public class AreaTest extends Father{
+	void test() {
+		
+	}
+	static {
+		System.out.println("abc1");	
+	}
+	public static void main(String[] args) {
+		System.out.println("abc2");	
+	}
+}
---
+++ src/oop/abst/AnonymouseTest.java
@@ -0,0 +1,16 @@
+package oop.abst;
+
+interface Viewer{
+	void show();
+	void off();
+}
+public class AnonymouseTest {
+
+	public static void test() throws Exception{
+		
+	}
+	public static void main(String[] args) {
+		String numStr = "일이삼";
+		System.out.println(Integer.parseInt(numStr));
+	}
+}
